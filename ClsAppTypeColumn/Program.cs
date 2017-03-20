5df9bbdc33029029604ec3ecb14c19d8229aebf5
fulviocanducci <fulviocanducci@hotmail.com>
2017-03-20 13:12:52 -0300

    Add .

--- ClsAppTypeColumn/Program.cs
+++ ClsAppTypeColumn/Program.cs
@@ -10,6 +10,16 @@ namespace ClsAppTypeColumn
     {
         static void Main(string[] args)
         {
+            DaoPeople dao = new DaoPeople(Connect.getSqlConnectionInstance);
+            People people = new People();
+            //people.Name = "Teste 1";
+            //people.TimeCreated = TimeSpan.Parse("13:10");
+            //people.GuidId = Guid.NewGuid();
+            //people.Value = 125.36M;
+            //people.DateCreated = DateTime.Now.Date;
+            //people.Active = false;
+
+            people = dao.Add(people);
         }
     }
 }
---
+++ ClsAppTypeColumn/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ClsAppTypeColumn
+{
+    public static class Utils
+    {
+        public static object GetValueOrDBNull<T>(this T? _o)
+            where T: struct
+        {
+            return _o.HasValue
+                ? (object)_o
+                : DBNull.Value;
+        }
+
+        public static object GetValueOrDBNull(this string _o)            
+        {
+            return (object)_o ?? DBNull.Value;
+        }
+
+        public static void IsOpen(this SqlConnection _c)
+        {
+            if (_c.State == ConnectionState.Closed)
+                _c.Open();
+        }
+
+        public async static Task IsOpenAsync(this SqlConnection _c)
+        {
+            if (_c.State == ConnectionState.Closed)
+                await _c.OpenAsync();
+        }
+
+        public static SqlCommand OpenAndCreateCommand(this SqlConnection _c)
+        {
+            _c.IsOpen();
+            return _c.CreateCommand();
+        }
+
+        public async static Task<SqlCommand> OpenAsyncAndCreateCommand(this SqlConnection _c)
+        {
+            await _c.IsOpenAsync();
+            return _c.CreateCommand();
+        }
+    }
+}
